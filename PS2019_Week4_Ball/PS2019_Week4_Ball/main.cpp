2bc2eef637d28d89acb8467e2ad9c58acbd4fab1
middlebro <middlebro@inha.edu>
2019-04-07 13:19:48 +0900

    Add Week4_Ball problem solve project.

---
+++ PS2019_Week4_Ball/PS2019_Week4_Ball/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PS2019_Week4_Ball
+//
+//  Created by 서형중 on 07/04/2019.
+//  Copyright © 2019 Hyeongjung Seo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
