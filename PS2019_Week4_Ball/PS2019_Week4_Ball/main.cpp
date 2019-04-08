8276dd56f0f262a3675852e117e92871eccb42cd
middlebro <middlebro@inha.edu>
2019-04-08 18:31:24 +0900

    Solve for Week4 Ball problem

--- PS2019_Week4_Ball/PS2019_Week4_Ball/main.cpp
+++ PS2019_Week4_Ball/PS2019_Week4_Ball/main.cpp
@@ -7,9 +7,28 @@
 //
 
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ios::sync_with_stdio(false);
+    int t;      // t < 50
+    cin >> t;
+    while (t--) {
+        int n, l, k;
+        cin >> n >> l >> k;
+        vector<int> v;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            if (x > 0)
+                v.push_back(l - x);
+            else
+                v.push_back(abs(x));
+        }
+        sort(v.begin(), v.end());
+        cout << v[k - 1] << endl;
+    }
     return 0;
 }
