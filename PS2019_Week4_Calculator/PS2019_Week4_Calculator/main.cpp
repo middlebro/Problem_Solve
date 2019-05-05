d552d3a54687458d97f9856bae1ec16ae83ca4ac
middlebro <middlebro@inha.edu>
2019-05-05 19:31:52 +0900

    add Week4 Calculator proj. init

---
+++ PS2019_Week4_Calculator/PS2019_Week4_Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PS2019_Week4_Calculator
+//
+//  Created by 서형중 on 05/05/2019.
+//  Copyright © 2019 Hyeongjung Seo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
