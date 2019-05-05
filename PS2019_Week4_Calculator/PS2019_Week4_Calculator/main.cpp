9dcc3a75d9d3aa2f05445ff9353e68c751fd58e6
middlebro <middlebro@inha.edu>
2019-05-06 01:15:02 +0900

    insert code except case of ')'

--- PS2019_Week4_Calculator/PS2019_Week4_Calculator/main.cpp
+++ PS2019_Week4_Calculator/PS2019_Week4_Calculator/main.cpp
@@ -7,9 +7,60 @@
 //
 
 #include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int calculate(int a, int b, char op) {
+    if (op == '+')
+        return a + b;
+    else if (op == '-')
+        return a - b;
+    else
+        return NULL;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while (t--) {
+        char input;
+        cin >> input;
+        stack<char> op;
+        stack<int> number;
+        while (input != '$') {
+            if ((input == '(') || (input == '+') || (input == '-')) {
+                op.push(input);
+            }
+            else if (input == ')') {
+                while (!op.empty()) {
+                    if (op.top() == '(') {
+                        op.pop();
+                        break;
+                    }
+                    if (number.empty()) {
+                        cout << 'N' << endl;
+                        break;
+                    }
+                    int a = number.top();
+                    number.pop();
+                    if (number.size() == 0) {
+                        op.pop();
+                        number.push(a);
+                    }
+                    else {
+                        
+                    }
+                }
+            }
+            else
+                number.push(atoi(&input));
+        }
+        if (number.size() == 1 && op.empty())
+            cout << number.top() << endl;
+        else
+            cout << 'N' << endl;
+    }
     return 0;
 }
