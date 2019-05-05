fe3616e47cdc190900f6ab6cb320c4ed856413e5
middlebro <middlebro@inha.edu>
2019-05-06 01:15:51 +0900

    Solve for Week8 AVL.

--- PS2019_Week8_AVL/PS2019_Week8_AVL/main.cpp
+++ PS2019_Week8_AVL/PS2019_Week8_AVL/main.cpp
@@ -6,141 +6,329 @@
 //  Copyright © 2019 Hyeongjung Seo. All rights reserved.
 //
 
-#include <iostream>
-#include <cstdlib>
-#include <vector>
+//##define _CRT_SECURE_NO_WARNINGS
 #include <string>
-#include <algorithm>
+#include<iostream>
 using namespace std;
+#define _max(x,y) ((x > y) ? x : y)
 
-struct Node {
-    int std_num;
-    string name;
-    Node* par;
-    Node* left;
-    Node* right;
-    Node(int key, string value) : std_num(key), name(value), par(NULL), left(NULL), right(NULL) {}
-    Node() : std_num(NULL), name(NULL),par(NULL), left(NULL), right(NULL) {}
+// class Node 정의
+class Node
+{
+public:
+    int height;
+    int key;
+    string value;
+    Node *parent, *left, *right;
+    Node() {                    // 생성자1
+        key = height = 0;
+        value = "";
+        parent = left = right = NULL;
+    };
+    Node(int key, string value) {    // 생성자2
+        this->key = key;
+        this->value = value;
+        parent = left = right = NULL;
+    };
 };
 
-struct AVL {
-    Node* root;
-    int get_height(Node*);
-    int get_diff(Node*);
-    Node* find(Node*, int);
-    void insert(int, string);
-    Node* rotation_RR(Node*);
-    Node* rotation_LL(Node*);
-    Node* rotation_RL(Node*);
-    Node* rotation_LR(Node*);
-    Node* restructure(Node*);
-    AVL() : root(NULL) {}
+// class AVLTree (AVL 트리) 정의
+class AVLTree
+{
+public:
+    Node *root;
+    int n;
+    AVLTree() {        // 생성자
+        this->root = NULL;
+        n = 0;
+    };
+    Node* find( int key, Node* v);
+    Node* insert( int key,  string value);
     
+    int height(Node* v);
+    void setHeight(Node* v);
+    bool isBalanced( Node *v);
+    Node* tallGrandChild( Node* v);
+    Node* restructure(Node* v);
+    void rebalance(Node* v);
+    bool isExternal(Node* v);
+    Node* rotation_RR(Node* v);
+    Node* rotation_LL(Node* v);
+    Node* rotation_LR(Node* v);
+    Node* rotation_RL(Node* v);
+    
+    int size();
+    bool empty();
 };
-int AVL::get_height(Node* Node) {
-    int height = 0;
-    if (Node != NULL)
-        height = 1 + max(get_height(Node->left), get_height(Node->right));
-    return height;
+
+// AVL 트리의 size 반환
+int AVLTree::size() {
+    return n;
 }
-int AVL::get_diff(Node* Node) {
-    if (Node == NULL)
-        return 0;
-    return get_height(Node->left) - get_height(Node->right);
+
+// AVL 트리의 empty 여부 반환
+bool AVLTree::empty() {
+    return (n == 0);
 }
-Node* AVL::find(Node* node, int key) {
-    if (node->left == NULL && node->right == NULL) return node;
-    if (node->std_num > key)
-        return find(node->left, key);
-    else if(node->std_num <= key)
-        return find(node->right, key);
+
+// 노드v가 leaf 노드인지 확인
+bool AVLTree::isExternal(Node* v) {
+    return (v->left == NULL && v->right == NULL);
+}
+
+// 노드 v의 높이 반환
+int AVLTree::height(Node* v) {
+    if (v == NULL)
+        return -1;
     else
-        return node;
+        return (isExternal(v)) ? 0 : v->height;
 }
-void AVL::insert(int key, string value) {
-    Node* curLeafNode = find(root, key);
-    Node* newNode = new Node(key, value);
-    if (key < curLeafNode->std_num)
-        curLeafNode->left = newNode;
-    else
-        curLeafNode->right = newNode;
-    newNode->par = curLeafNode;
-    restructure(newNode);
-    cout << value << " " << get_height(newNode) << endl;
+
+// 노드 v의 높이 계산
+void AVLTree::setHeight(Node* v) {
+    int hl = height(v->left);    // height of left child
+    int hr = height(v->right);    // height of right child
+    v->height = 1 + _max(hl, hr);
 }
-Node* AVL::rotation_RR(Node* par) {
+
+//  노드 v에 대해 균형 여부 확인
+bool AVLTree::isBalanced( Node *v) {
+    int bal = height(v->left) - height(v->right);
+    return ((-1 <= bal) && (bal <= 1));
+}
+
+// 최초의 불균형 노드 z에 대해, height가 더 큰, z의 grandchld 탐색
+Node* AVLTree::tallGrandChild( Node* z) {
+    Node* zl = z->left;
+    Node* zr = z->right;
+    if (height(zl) >= height(zr)) {
+        if (height(zl->left) >= height(zl->right))    // 자식과 같은 방향의 손자를 우선적으로 선택
+            return zl->left;
+        else
+            return zl->right;
+    }
+    else {
+        if (height(zr->right) >= height(zr->left))    // 자식과 같은 방향의 손자를 우선적으로 선택
+            return zr->right;
+        else
+            return zr->left;
+    }
+}
+
+
+
+/****************************************************************************************************************************************///학생이 구현.
+Node* AVLTree::rotation_RR(Node* par) {
     Node* chi = par->right;
     par->right = chi->left;
     chi->left = par;
     return chi;
 }
-Node* AVL::rotation_LL(Node* par) {
+Node* AVLTree::rotation_LL(Node* par) {
     Node* chi = par->left;
     par->left = chi->right;
     chi->right = par;
     return chi;
 }
-Node* AVL::rotation_RL(Node* par) {
+Node* AVLTree::rotation_RL(Node* par) {
     Node* chi = par->right;
     par->right = rotation_LL(chi);
     return rotation_RR(par);
 }
-Node* AVL::rotation_LR(Node* par) {
+Node* AVLTree::rotation_LR(Node* par) {
     Node* chi = par->left;
     par->left = rotation_RR(chi);
     return rotation_LL(par);
 }
-
-Node* AVL::restructure(Node * x) {
-    if (x->par == NULL || x->par->par == NULL)
+Node* AVLTree::restructure(Node* x) {
+    if (x->parent == NULL || x->parent->parent == NULL)
         return NULL;
-    Node* y = x->par;
-    Node* z = y->par;
-    if (x == y->left) {
-        if (y == z->left)
-            return rotation_LL(z);
-        else
-            return rotation_RL(z);
+    Node* y = x->parent;
+    Node* z = y->parent;
+    if (y->left == x) {
+        if (z->left == y) {
+            if (z->parent != NULL) {
+                if (z->parent->left == z)
+                    z->parent->left = y;
+                else
+                    z->parent->right = y;
+            }
+            y->parent = z->parent;
+            z->parent = y;
+            z->left = y->right;
+            if (y->right != NULL)
+                y->right->parent = z;
+            y->right = z;
+            return y;
+        }
+        else {
+            if (z->parent != NULL) {
+                if (z->parent->left == z)
+                    z->parent->left = x;
+                else
+                    z->parent->right = x;
+            }
+            x->parent = z->parent;
+            z->parent = y->parent = x;
+            z->right = x->left;
+            if (x->left != NULL)
+                x->left->parent = z;
+            y->left = x->right;
+            if (x->right != NULL)
+                x->right->parent = y;
+            x->left = z;
+            x->right = y;
+            return x;
+        }
     }
     else {
-        if (y == z->left)
-            return rotation_LR(z);
-        else
-            return rotation_RR(z);
+        if (z->right == y) {
+            if (z->parent != NULL) {
+                if (z->parent->right == z)
+                    z->parent->right = y;
+                else
+                    z->parent->left = y;
+            }
+            y->parent = z->parent;
+            z->parent = y;
+            z->right = y->left;
+            if (y->left != NULL)
+                y->left->parent = z;
+            y->left = z;
+            return y;
+        }
+        else {
+            if (z->parent != NULL) {
+                if (z->parent->right == z)
+                    z->parent->right = x;
+                else
+                    z->parent->left = x;
+            }
+            x->parent = z->parent;
+            z->parent = y->parent = x;
+            z->left = x->right;
+            if (x->right != NULL)
+                x->right->parent = z;
+            y->right = x->left;
+            if (x->left != NULL)
+                x->left->parent = y;
+            x->right = z;
+            x->left = y;
+            return x;
+        }
     }
-    return NULL;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    AVL tree;
-    while (n--) {
-        string cmd, val;
-        int k;
-        cin >> cmd;
-        if (cmd == "insert") {
-            cin >> k >> val;
-            if (tree.root != NULL)
-                tree.insert(k, val);
-            else {
-                Node* tmp = new Node(k, val);
-                tree.root = tmp;
-                cout << val << " " << 0 << endl;
-            }
-        }
-        else if (cmd == "find") {
-            cin >> k;
-            Node* tmp = tree.find(tree.root, k);
-            if (tmp->left == NULL && tmp->right == NULL) {
-                cout << -1 << endl;
-            }
-            else {
-                cout << tmp->std_num << tree.get_height(tmp) << endl;
-            }
+/*****************************************************************************************************************************************///학생이 구현
+
+
+
+
+
+// 노드 v부터 root 노드까지의 path 상에 노드의 height를 재설정하고 균형여부 확인
+// 불균형 노드가 있으면, restructuring
+void AVLTree::rebalance(Node* v) {
+    Node *z = v;
+    while (z->parent != NULL) {
+        z = z->parent;
+        setHeight(z);
+        if (!isBalanced(z)) {
+            Node* x = tallGrandChild(z);
+            z = restructure(x);        // -------------------> restructure() 를 직접 구현해야합니다.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            setHeight(z->left);
+            setHeight(z->right);
+            setHeight(z);
         }
     }
+    root = z;
+}
+
+
+
+//key가 삽입될 위치 반환
+Node* AVLTree::find( int key, Node* v) {
+    Node* leafNode;
+    leafNode = NULL;
     
+    while (v != NULL) {
+        leafNode = v;
+        if (key < v->key)
+            v = v->left;
+        else if (key > v->key)
+            v = v->right;
+        else
+            return v;
+    }
+    
+    // leaf까지 도달한 경우, leaf노드를 반환한다.
+    return leafNode;
+}
+
+
+//노드 삽입
+Node* AVLTree::insert( int key,  string value) {
+    Node *curLeafNode = find(key, root);
+    Node *newNode = new Node(key, value);
+    Node *insertNode = new Node();
+    
+    if (key < curLeafNode->key)    // 삽입하려는 key가 현재 leaf노드의 key보다 작은 경우
+    {
+        curLeafNode->left = newNode;
+    }
+    else     // 삽입하려는 key가  현재 leaf노드의 key보다 큰 경우
+    {
+        curLeafNode->right = newNode;
+    }
+    newNode->parent = curLeafNode;
+    n++;
+    
+    // restructure 및 height 갱신
+    setHeight(newNode);
+    rebalance(newNode);
+    
+    
+    // 삽입된 AVL tree의 노드의 height 출력
+    insertNode = find(key, root);
+    cout << insertNode->value << " " << insertNode->height << endl;
+    return insertNode;
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int t, key;
+    string instruct;
+    AVLTree T;
+    
+    cin >> t;
+    while (t--) {
+        Node* tempNode;
+        cin >> instruct;
+        if (instruct=="find") {
+            
+            cin >> key;
+            
+            ///학생이 구현.
+            // 해당 입력에 대해 문제의 예시처럼  출력되도록 구현하세요.
+            ///학생이 구현.
+            tempNode = T.find(key, T.root);
+            if (tempNode == NULL)
+                cout << -1 << endl;
+            else
+                cout << tempNode->value << " " << tempNode->height << endl;
+        }
+        
+        else if (instruct=="insert") {
+            cin >> key >> instruct;
+            if (T.empty()) {
+                T.root = new Node(key, instruct);
+                T.n++;
+                cout << T.root->value << " " << T.root->height << endl;
+            }
+            else
+                T.insert(key, instruct);
+        }
+    }
     return 0;
 }
