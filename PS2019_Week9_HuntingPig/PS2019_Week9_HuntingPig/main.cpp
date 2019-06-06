ad91e7433c0b7711aa70a8042196b861620124c4
middlebro <middlebro@inha.edu>
2019-06-06 16:00:24 +0900

    Solve for Week 9 HuntingPig init.

---
+++ PS2019_Week9_HuntingPig/PS2019_Week9_HuntingPig/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  PS2019_Week9_HuntingPig
+//
+//  Created by 서형중 on 06/06/2019.
+//  Copyright © 2019 Hyeongjung Seo. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < n; ++i) {
+            int h;
+            cin >> h;
+        }
+        for (int i = 0; i < m; ++i) {
+            int p1, p2;
+            cin >> p1 >> p2;
+            // init dist[p1][p2] = 1
+        }
+    }
+    return 0;
+}
