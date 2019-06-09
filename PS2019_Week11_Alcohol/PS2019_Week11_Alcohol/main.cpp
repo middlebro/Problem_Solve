337b220fb36107ee4c10aa315f4cbb6aedbe8227
middlebro <middlebro@inha.edu>
2019-06-10 00:26:55 +0900

    Solve for Week 11 Alcohol : CORRECT

--- PS2019_Week11_Alcohol/PS2019_Week11_Alcohol/main.cpp
+++ PS2019_Week11_Alcohol/PS2019_Week11_Alcohol/main.cpp
@@ -7,23 +7,32 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+double glass[401][80201];
+int main() {
     ios::sync_with_stdio(false);
+    
     int t;
     cin >> t;
     while (t--) {
         int b, l, n;
         cin >> b >> l >> n;
-        double glass[401][401] = {{0}};
+        
+        // init glass
+        for (int i = 1; i <= l; ++i) {
+            for (int j = 1; j <= i*(i + 1)/2; ++j) {
+                glass[i][j] = 0;
+            }
+        }
         glass[1][1] = 7 * b;
         
-        for (int i = 1; i <= l ; ++i) {
+        for (int i = 1; i < l; ++i) {
             int number = 1;
             for (int j = 1; j <= i; ++j) {
                 for (int k = 1; k <= j; ++k) {
-                    double dump = glass[i][number]-1;
+                    double dump = glass[i][number] - 1;
                     if (dump > 0) {
                         glass[i + 1][number] += dump / 3;
                         glass[i + 1][number + j] += dump / 3;
@@ -33,15 +42,13 @@ int main(int argc, const char * argv[]) {
                 }
             }
         }
-        
-        int answer;
+
         if (glass[l][n] >= 1)
-            answer = 1;
-        if (glass[l][n] > 0 && glass[l][n] < 1)
-            answer = 2;
-        if (glass[l][n] == 0)
-            answer = 0;
-        cout << answer << "\n";
+            cout << 1 << "\n";
+        else if (glass[l][n] <= 0)
+            cout << 0 << "\n";
+        else
+            cout << 2 << "\n";
     }
     return 0;
 }
