757415e991f4d07821208af490c346656f42c7f8
middlebro <middlebro@inha.edu>
2019-06-10 13:01:39 +0900

    Solve for Week 12 Egg_Carton : CORRECT

--- PS2019_Week12_Egg_Carton/PS2019_Week12_Egg_Carton/main.cpp
+++ PS2019_Week12_Egg_Carton/PS2019_Week12_Egg_Carton/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    std::ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int solution[100001];
+        
+        for (int i = 1; i <= n; ++i) {
+            solution[i] = 1000000;
+        }
+
+        vector<int> carton;
+        for (int i = 0; i < m; ++i) {
+            int size_of_carton;
+            cin >> size_of_carton;
+            carton.push_back(size_of_carton);
+        }
+
+        for (int i = 0; i < carton.size(); ++i) {
+            for (int j = carton[i]; j <= n; ++j) {
+                int rem = j - carton[i];
+                if (rem != 0) {
+                    int sum = solution[rem] + 1;
+                    if (sum < solution[j])
+                        solution[j] = sum;
+                }
+                else
+                    solution[j] = 1;
+            }
+        }
+        
+        if (solution[n] < 1000000)
+            cout << solution[n] << "\n";
+        else
+            cout << "0\n";
+    }
     return 0;
 }
